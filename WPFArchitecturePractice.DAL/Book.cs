c9fe1eb0d57e8704064910e6b9735d58394890c0
JMR <853320864@qq.com>
2024-04-02 16:56:36 +0800

    update Entity

---
+++ WPFArchitecturePractice.DAL/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace WPFArchitecturePractice.DAL;
+
+    public class Book
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public virtual BookCategory Category { get; set; }
+
+        public virtual bool isRentingState { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+    }
---
+++ WPFArchitecturePractice.DAL/WPFArchitecturePraticeContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFArchitecturePractice.DAL
+{
+    public class WPFArchitecturePraticeContext:DbContext
+    {
+        public DbSet<Book> Books => Set<Book>();
+        public DbSet<BookCategory> BookCategories => Set<BookCategory>();
+
+    }
+}
